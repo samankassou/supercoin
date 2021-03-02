861f8651b23a61a4ce28c0837a337dd56c385f34
samankassou <samankassoufoulla@gmail.com>
2021-03-02 18:58:32 +0100

    Show transaction details for deposits and withdrawals in the user dashboard

--- resources/views/users/deposits.blade.php
+++ resources/views/users/deposits.blade.php
@@ -1,24 +1,41 @@
 @extends('layouts.users.app', ['title' => 'Deposits'])
 
 @section('content')
-<div class="row justify-content-center">
-  <div class="col-md-6">
-    <!-- /.col -->
-     @foreach ($deposits as $deposit)
-     <div class="info-box mb-3 text-success">
-      <span class="info-box-icon"><i class="fas fa-exchange-alt"></i></span>
-  
-      <div class="info-box-content">
-        <span class="info-box-text">Deposit</span>
-        <span class="info-box-number">$ {{ $deposit->amount }}</span>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="row mb-4 justify-content-end">
+        <button class="btn btn-success btn-sm">New</button>
       </div>
-      <!-- /.info-box-content -->
+      <!-- /.col -->
+       @foreach ($deposits as $deposit)
+       <div class="info-box mb-3 text-success">
+        <span class="info-box-icon"><i class="fas fa-exchange-alt"></i></span>
+    
+        <div class="info-box-content">
+          <div class="row justify-content-between">
+            <span class="info-box-text">
+              deposit
+            </span>
+            <span class="text-sm">{{ $deposit->created_at->format('d M Y, H:i:s') }}</span>
+          </div>
+          <div class="row justify-content-between">
+            <span class="info-box-number">$ {{ $deposit->amount }}</span>
+            <span class="info-box-text">{{ $deposit->status }}</span>
+          </div>
+          @if ($deposit->status === "processing")
+              <div class="row justify-content-end">
+                <a class="btn btn-danger btn-xs" href="#">Cancel</a>
+              </div>
+          @endif
+          
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+      <!-- /.info-box -->
+       @endforeach
     </div>
-    <!-- /.info-box -->
-     @endforeach
   </div>
-</div>
-<div class="row justify-content-center">
-  {{ $deposits->links() }}
-</div>
+  <div class="row justify-content-center">
+    {{ $deposits->links() }}
+  </div>
 @endsection
--- resources/views/users/withdrawals.blade.php
+++ resources/views/users/withdrawals.blade.php
@@ -1,24 +1,41 @@
 @extends('layouts.users.app', ['title' => 'Withdrawals'])
 
 @section('content')
-<div class="row justify-content-center">
-  <div class="col-md-6">
-    <!-- /.col -->
-     @foreach ($withdrawals as $withdrawal)
-     <div class="info-box mb-3 text-danger">
-      <span class="info-box-icon"><i class="fas fa-exchange-alt"></i></span>
-  
-      <div class="info-box-content">
-        <span class="info-box-text">Withdrawal</span>
-        <span class="info-box-number">$ {{ $withdrawal->amount }}</span>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="row mb-4 justify-content-end">
+        <button class="btn btn-success btn-sm">New</button>
       </div>
-      <!-- /.info-box-content -->
+      <!-- /.col -->
+       @foreach ($withdrawals as $withdrawal)
+       <div class="info-box mb-3 text-danger">
+        <span class="info-box-icon"><i class="fas fa-exchange-alt"></i></span>
+    
+        <div class="info-box-content">
+          <div class="row justify-content-between">
+            <span class="info-box-text">
+              Withdrawal
+            </span>
+            <span class="text-sm">{{ $withdrawal->created_at->format('d M Y, H:i:s') }}</span>
+          </div>
+          <div class="row justify-content-between">
+            <span class="info-box-number">$ {{ $withdrawal->amount }}</span>
+            <span class="info-box-text">{{ $withdrawal->status }}</span>
+          </div>
+          @if ($withdrawal->status === "processing")
+              <div class="row justify-content-end">
+                <a class="btn btn-danger btn-xs" href="#">Cancel</a>
+              </div>
+          @endif
+          
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+      <!-- /.info-box -->
+       @endforeach
     </div>
-    <!-- /.info-box -->
-     @endforeach
   </div>
-</div>
-<div class="row justify-content-center">
-  {{ $withdrawals->links() }}
-</div>
+  <div class="row justify-content-center">
+    {{ $withdrawals->links() }}
+  </div>
 @endsection
